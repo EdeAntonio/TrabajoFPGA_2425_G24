c6e4d2bdab24f9cb30904cdd6662400baebce401
Esther Aragoneses Hernández <163413466+estheraragoneses@users.noreply.github.com>
2025-01-18 19:33:56 +0100

    Comentar  Posicion.vhdl

--- ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/Posicion.vhdl
+++ ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/Posicion.vhdl
@@ -16,7 +16,7 @@ end Position;
 
 architecture Behavioral of Position is
     signal tup : std_logic := '0';                -- Señal interna para el temporizador de actualización
-    type state is (P0, P1, P2, P3);                -- Tipo enumerado para representar los estados
+    type state is (P0, P1, P2, P3);                -- Se representan los estados de las plantas (P0 = planta 0; P1 = planta 1; P2 = planta 2; P3 = planta 3)
     signal currentpos, nextpos : state := P0;    -- Señales internas para el estado actual y el siguiente
 begin
     t1 <= tup;                                    -- Asignación de la señal interna `tup` a la salida `t1`
@@ -35,69 +35,69 @@ begin
         end if;
     end process;
     
-    stateregister: process(tup)
+    stateregister: process(tup)                     -- Proceso para actualizar el estado actual según la señal `tup`
     begin
-        if tup = '1' then
-            currentpos <= nextpos;
+        if tup = '1' then                           -- Sólo actualiza el estado cuando `tup` está en 1
+            currentpos <= nextpos;                   -- Se avanza la posición actual a la siguiente
         end if;
     end process;
     
-    statedecoder: process(move, currentpos)
+    statedecoder: process(move, currentpos)           -- Proceso que decide la planta siguiente según la planta actual y la entrada `move` (00 = quieto; 10 = subir; 01= bajar)
     begin
         case currentpos is
-            when P0 =>
-                if move = "00" then
+            when P0 =>                                -- Si el ascensor está en la planta baja: 
+                if move = "00" then                        --Si no se realiza ningún movimiento se mantiene en la planta 0
+                    nextpos <= P0;                         
+                elsif move = "10" then                     --Si se realiza un movimeinto ascendente el ascensor sube a la planta 1
+                    nextpos <= P1;                    
+                else
+                    nextpos <= P0;                         -- En cualquier otro caso el ascensor permanece en la planta en la que está
+                end if;
+            when P1 =>                                -- Si el ascensor está en la planta 1: 
+                if move = "00" then                         --Si no se realiza ningún movimiento se mantiene en la planta 1
+                    nextpos <= P1;
+                elsif move = "10" then                      --Si se realiza un movimeinto ascendente el ascensor sube a la planta 2
+                    nextpos <= P2;
+                elsif move = "01" then                      --Si se realiza un movimeinto descendente el ascensor baja a la planta 0
                     nextpos <= P0;
-                elsif move = "10" then
-                    nextpos <= P1;
-                else
-                    nextpos <= P0;
-                end if;
-            when P1 =>
-                if move = "00" then 
-                    nextpos <= P1;
-                elsif move = "10" then
-                    nextpos <= P2;
-                elsif move = "01" then 
-                    nextpos <= P0;
-                else
+                else                                        -- En cualquier otro caso el ascensor permanece en la planta en la que está
                     nextpos <= P1;
                 end if;
-            when P2 =>
-                if move = "00" then 
+            when P2 =>                                 -- Si el ascensor está en la planta 2: 
+                if move = "00" then                         --Si no se realiza ningún movimiento se mantiene en la planta 2
                     nextpos <= P2;
-                elsif move = "10" then
+                elsif move = "10" then                      --Si se realiza un movimeinto ascendente el ascensor sube a la planta 3
                     nextpos <= P3;
-                elsif move = "01" then 
+                elsif move = "01" then                       --Si se realiza un movimeinto descendente el ascensor baja a la planta 1
                     nextpos <= P1;
-                else
+                else                                          -- En cualquier otro caso el ascensor permanece en la planta en la que está
                     nextpos <= P2;
                 end if;
-            when P3 =>
-                if move = "00" then
+            when P3 =>                                -- Si el ascensor está en la planta 3: 
+                if move = "00" then                           --Si no se realiza ningún movimiento se mantiene en la planta 3
                     nextpos <= P3;
-                elsif move = "01" then
+                elsif move = "01" then                        --Si se realiza un movimeinto descendente el ascensor baja a la planta 2
                     nextpos <= P2;
-                else
+                else                                          -- En cualquier otro caso el ascensor permanece en la planta en la que está
                     nextpos <= P3;
                 end if;
-            when others => nextpos <= currentpos;
+            when others => nextpos <= currentpos;            -- Opción por defecto en caso de error (se mantiene en la posición actual si hay un fallo)
         end case;
     end process;
     
-    outdecoder: process(currentpos)
+    outdecoder: process(currentpos)                            -- Proceso para decodificar el estado actual (planta) y asignarlo a la salida `pos`
     begin
         case currentpos is
             when P0 =>
-                pos <= "00";
+                pos <= "00";                            -- Planta baja representada como "00"
             when P1 =>
-                pos <= "01";
+                pos <= "01";                            -- Planta 1 representada como "01"
             when P2 =>
-                pos <= "10";
+                pos <= "10";                            -- Planta 2 representada como "10"
             when P3 => 
-                pos <= "11";
+                pos <= "11";                            -- Planta 3 representada como "11"
             when others =>
-                pos <= "00";
+                pos <= "00";                            -- Valor por defecto: planta baja
         end case;
      end process;
 end Behavioral;
