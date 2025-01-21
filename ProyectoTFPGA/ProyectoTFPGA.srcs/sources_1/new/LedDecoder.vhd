9b7960f259d67749d3c884d5b19b6bc32629873e
Enrique de Antonio <enriquedeantoniosanchez@gmail.com>
2025-01-21 17:22:03 +0100

    Cambio de nombre, arreglo de fallos y creación de una menor frecuencia

--- ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/LedDecoder.vhd
+++ ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/LedDecoder.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 19.01.2025 19:31:58
+-- Create Date: 19.01.2025 19:29:25
 -- Design Name: 
--- Module Name: LedDecoder - Behavioral
+-- Module Name: LedGestor - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -22,50 +22,32 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity LedGestor is
+entity LedDecoder is
     port(
-        clk: in std_logic;
-        elev_led: in std_logic_vector(6 downto 0);
-        move_led: in std_logic_vector(6 downto 0);
-        place_led: in std_logic_vector(6 downto 0);
         pos: in std_logic_vector(1 downto 0);
-        led_on: out std_logic_vector(6 downto 0);
-        display_on: out std_logic_vector(7 downto 0)
+        move: in std_logic_vector(1 downto 0);
+        door: in std_logic;
+        elev_led: out std_logic_vector(6 downto 0); --display del ascensor
+        place_led: out std_logic_vector(6 downto 0); --display de la posicion
+        move_led: out std_logic_vector(6 downto 0) -- display del movimiento
         );
-end LedGestor;
+end LedDecoder;
 
-architecture Behavioral of LedGestor is
-    signal led_an: std_logic_vector (7 downto 0) := "00110000";
-
+architecture dataflow of LedDecoder is
 begin
-    p1: process (pos)
-    begin
-        case pos is
-            when "00" => led_an <= "11001110";
-            when "01" => led_an <= "11001101";
-            when "10" => led_an <= "11001011";
-            when "11" => led_an <= "11000111";
-            when others => led_an <= "01001111";
-        end case;
-    end process;
-    
-    p2: process (clk)
-    variable count: integer := 0;
-    begin
-    if rising_edge(clk) then
-        display_on <= (others => '1');
-        display_on(count) <= led_an(count);
-        if -1 < count and count < 4 then
-            led_on <= elev_led;
-        elsif count = 4 then
-            led_on <= place_led;
-        elsif count = 5 then
-            led_on <= move_led;
-        elsif count = 7 then
-            led_on <= "0110000";
-            count := -1;
-        end if;
-        count := count + 1;
-     end if;
-     end process;
-end Behavioral;
+    with move select -- Selección del movimiento indicado en el display
+        move_led <= "0001111" when "10", -- 7 indica hacia arriba
+                    "1110001" when "01", -- L incia hacia abajo
+                    "1111110" when "00", -- - indica parado
+                    "0110000" when others; -- E de error
+    with door select -- Selección del formato de display del ascensor
+        elev_led <= "0000001" when '1', -- 0, un cuadrado abierto
+                    "0000000" when '0', -- 8, un cuadrado con una linea en medio
+                    "0110000" when others;
+    with pos select -- Selección del piso indicado en el display
+        place_led <= "0000001" when "00",
+                     "1001111" when "01",
+                     "0010010" when "10",
+                     "0000110" when "11",
+                     "0110000" when others;
+end dataflow;
--- ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/LedGestor.vhd
+++ ProyectoTFPGA/ProyectoTFPGA.srcs/sources_1/new/LedGestor.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 19.01.2025 19:29:25
+-- Create Date: 19.01.2025 19:31:58
 -- Design Name: 
--- Module Name: LedGestor - Behavioral
+-- Module Name: LedDecoder - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -22,32 +22,72 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity LedDecoder is
-    port(
-        pos: in std_logic_vector(1 downto 0);
-        move: in std_logic_vector(1 downto 0);
-        door: in std_logic;
-        elev_led: out std_logic_vector(6 downto 0); --display del ascensor
-        place_led: out std_logic_vector(6 downto 0); --display de la posicion
-        move_led: out std_logic_vector(6 downto 0) -- display del movimiento
+entity LedGestor is
+    generic(
+        timelimit: integer := 10**8
         );
-end LedDecoder;
+    port(
+        clk: in std_logic;
+        elev_led: in std_logic_vector(6 downto 0);
+        move_led: in std_logic_vector(6 downto 0);
+        place_led: in std_logic_vector(6 downto 0);
+        pos: in std_logic_vector(1 downto 0);
+        led_on: out std_logic_vector(6 downto 0);
+        display_on: out std_logic_vector(7 downto 0)
+        );
+end LedGestor;
 
-architecture dataflow of LedDecoder is
+architecture Behavioral of LedGestor is
+    signal led_an: std_logic_vector (7 downto 0) := "00110000";
+    signal count: integer := 0;
+    signal onOut: std_logic_vector(6 downto 0);
+    signal tup: std_logic;
 begin
-    with move select -- Selección del movimiento indicado en el display
-        move_led <= "0001111" when "10", -- 7 indica hacia arriba
-                    "1110001" when "01", -- L incia hacia abajo
-                    "1111110" when "00", -- - indica parado
-                    "0110000" when others; -- E de error
-    with door select -- Selección del formato de display del ascensor
-        elev_led <= "0000001" when '1', -- 0, un cuadrado abierto
-                    "0000000" when '0', -- 8, un cuadrado con una linea en medio
-                    "0110000" when others;
-    with pos select -- Selección del piso indicado en el display
-        place_led <= "0000001" when "00",
-                     "1001111" when "01",
-                     "0010010" when "10",
-                     "0000110" when "11",
-                     "0110000" when others;
-end dataflow;
+    FqAdt: process(clk)
+    variable timecount : integer := 0;        -- Variable interna para contar el tiempo
+    begin
+        if rising_edge(clk) then                -- Se ejecuta en el flanco de subida del reloj
+            timecount := timecount + 1;           -- Incrementa el contador de tiempo
+            if timecount = (timelimit/(2*10**3)) then        
+                tup <= '0';                        -- Señal `tup` en bajo después del primer intervalo
+            end if;
+            if timecount = (timelimit/10**3) then
+                timecount := 0;                    -- Reinicia el contador después de 2 intervalos
+                tup <= '1';                        -- Señal `tup` en alto para indicar actualización
+                if count > 7 then
+                    count <= 0;
+                else
+                    count <= count + 1;
+                end if;
+            end if;
+        end if;
+    end process;    
+    KDis: process (pos)
+    begin
+        case pos is
+            when "00" => led_an <= "11001110";
+            when "01" => led_an <= "11001101";
+            when "10" => led_an <= "11001011";
+            when "11" => led_an <= "11000111";
+            when others => led_an <= "01001111";
+        end case;
+    end process;
+    
+    SDis: process (count)
+    begin
+        display_on <= (others => '1');
+        display_on(count)<=led_an(count);                           
+        case count is
+            when 0|1|2|3 =>
+                led_on <= elev_led;
+            when 4 =>
+                led_on <= place_led;
+            when 5|6 => 
+                led_on <= move_led;
+            when 7 =>
+                led_on <= "0110000";
+            when others =>
+                led_on <= "0110000";
+        end case;
+    end process;
+end Behavioral;
